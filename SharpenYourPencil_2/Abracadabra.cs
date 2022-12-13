46528918203b1a5111ebcb2f7b192f339eafa237
chajus1 <chajus1@googlemail.com>
2022-12-13 00:23:27 +0000

    Add project files.

---
+++ SharpenYourPencil_2/Abracadabra.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpenYourPencil_2
+{
+    internal class Abracadabra
+    {
+        public int Vavavoom;
+
+        public bool Lala(int floq)
+        {
+            if(floq < Vavavoom)
+            {
+                Vavavoom += floq;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ SharpenYourPencil_2/Pizzazz.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpenYourPencil_2
+{
+    internal class Pizzazz
+    {
+        public int Zippo;
+
+        public void Bamboo(int eek)
+        {
+            Zippo += eek;
+        }
+    }
+}
